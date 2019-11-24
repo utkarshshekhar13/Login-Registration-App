75a887686d5ec9d1eaeade5de37199faab0ee153
Utkarsh Shekhar <utki.619@gmail.com>
2019-11-24 06:24:54 +0530

    Initial Commit

---
+++ Login Registration App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login Registration App
+//
+//  Created by Utkarsh Shekhar on 24/11/19.
+//  Copyright © 2019 Utkarsh Shekhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
